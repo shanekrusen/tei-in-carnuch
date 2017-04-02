e602848b11c3f8e3a8b659254d40d41d61bd5417
Shane Krusen <shanekrusen@gmail.com>
2017-04-01 21:02:48 -0400

    Initial Commit

---
+++ master.js
@@ -0,0 +1,35 @@
+$(document).ready(function() {
+    $('.body-content').hide();
+    $('.home').show();
+    $('.fade').hide();
+    setTimeout(function() {
+        $('.fade').fadeIn(1600);
+    }, 1000);
+    $('.enter').click(function() {
+        $('.overlay').fadeOut(1600);
+    });
+    $('.home-link').click(function() {
+        $('.about').hide();
+        $('.published').hide();
+        $('.contact').hide();
+        $('.home').show();
+    });
+    $('.about-link').click(function() {
+        $('.home').hide();
+        $('.published').hide();
+        $('.contact').hide();
+        $('.about').show();
+    });
+    $('.published-link').click(function() {
+        $('.home').hide();
+        $('.contact').hide();
+        $('.about').hide();
+        $('.published').show();
+    });
+    $('.contact-link').click(function() {
+        $('.home').hide();
+        $('.about').hide();
+        $('.published').hide();
+        $('.contact').show();
+    })
+});
